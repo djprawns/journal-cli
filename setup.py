49307eecc5ec72d9cc05a62b05457e047fe53d91
Pranav Sharma <pranav.sharma@fabhotels.com>
2019-08-14 05:44:13 +0530

    Adding more functionality, setup.py and Readme

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
 	name='journal-cli',             # This is the name of your PyPI-package.
-	version='0.1',                  # Update the version number for new release
+	version='0.3',                  # Update the version number for new release
 	scripts=['journal']             # The name of your scipt, and also the command you'll be using for calling it
 )
